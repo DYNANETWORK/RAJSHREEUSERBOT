c26dd352955c78c21e9360dbd732733c6848acdf
Sanskari787898 <93301854+Sanskari787898@users.noreply.github.com>
2021-12-11 22:41:54 +0530

    Update Config.py

--- userbot/Config.py
+++ userbot/Config.py
@@ -160,7 +160,7 @@ if ENV:
         # alive
         ALIVE_PIC = os.environ.get("ALIVE_PIC", None)
         ALIVE_MSG = os.environ.get("ALIVE_MSG", None)
-        HELP_PIC = os.environ.get("HELP_PIC", None)
+        HELP_PIC = os.environ.get("HELP_PIC", "https://telegra.ph/file/aa9a5ccf72dee00cd5614.jpg")
         BOT_PIC = os.environ.get("BOT_PIC", None)
         # auto bio
         BIO_MSG = os.environ.get("BIO_MSG", None)
